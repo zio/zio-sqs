0704e8e4f60ba8b7d3b1721d89ebecba42998d98
Grigorii Chudnov <g.chudnov@gmail.com>
2019-12-30 08:00:56 +0100

    start updated api

--- src/main/scala/zio/sqs/SqsProducer.scala
+++ src/main/scala/zio/sqs/SqsProducer.scala
@@ -6,6 +6,12 @@ import zio.stream.{ Stream, ZSink, ZStream }
 
 trait SqsProducer[T] {
 
+  // allow to fail the Task on failures
+  // 1. rename methods to the ones with E suffix
+  // 2. check that tests are running
+  // 3. create new methods without suffix that use methods with suffix and throw an error
+  // 4. add tests for the new methods
+
   def produce(e: SqsPublishEvent[T]): Task[SqsPublishErrorOrResult[T]]
 
   def produceBatch(es: Iterable[SqsPublishEvent[T]]): Task[List[SqsPublishErrorOrResult[T]]]

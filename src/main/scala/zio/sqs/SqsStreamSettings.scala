3610a4a5c6985ba7ee978879af964cad7c1582b7
Kevin Fleming <kvnflm@gmail.com>
2020-04-25 19:22:30 +0900

    Make visibilityTimeout and waitTimeSeconds settings optional
    A None value means that they won't get set on the AWS SDK request,
    so the queue's values will be used.

--- src/main/scala/zio/sqs/SqsStreamSettings.scala
+++ src/main/scala/zio/sqs/SqsStreamSettings.scala
@@ -4,8 +4,8 @@ case class SqsStreamSettings(
   attributeNames: List[String] = Nil,
   maxNumberOfMessages: Int = 1,
   messageAttributeNames: List[String] = Nil,
-  visibilityTimeout: Int = 30,
-  waitTimeSeconds: Int = 20,
+  visibilityTimeout: Option[Int] = None,
+  waitTimeSeconds: Option[Int] = None,
   autoDelete: Boolean = true,
   stopWhenQueueEmpty: Boolean = false
 )

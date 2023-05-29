cca83754101fc23aac62340e4dda7abcfbd48628
Vasileios Lampridis <vasileios.lampridis@comparethemarket.com>
2023-05-30 00:05:21 +0100

    Added tests for the method

--- zio-sqs/src/main/scala/zio/sqs/SqsStream.scala
+++ zio-sqs/src/main/scala/zio/sqs/SqsStream.scala
@@ -1,20 +1,17 @@
 package zio.sqs
 
-import zio.Schedule.WithState
 import zio.aws.sqs._
 import zio.aws.sqs.model._
 import zio._
 import zio.stream.ZStream
 import zio.aws.sqs.model.primitives.MessageAttributeName
 
-import java.time.OffsetDateTime
-
 object SqsStream {
 
   def apply(
-             queueUrl: String,
-             settings: SqsStreamSettings = SqsStreamSettings()
-           ): ZStream[Sqs, Throwable, Message.ReadOnly] = {
+    queueUrl: String,
+    settings: SqsStreamSettings = SqsStreamSettings()
+  ): ZStream[Sqs, Throwable, Message.ReadOnly] = {
 
     val request = ReceiveMessageRequest(
       queueUrl = queueUrl,
@@ -43,6 +40,7 @@ object SqsStream {
   /**
    * Processes SQS messages from the queue using a given function and
    * deletes the messages from the queue only if they processed successfully.
+   * This ensures that no message is lost in case of an error.
    *
    * If the message fails to be processed (=error or defect),
    * it will reappear in the queue after the `visibilityTimeout` expires.
@@ -54,33 +52,36 @@ object SqsStream {
    *
    * @param url                 the URL of the SQS queue
    * @param settings            the settings for the stream of messages from the queue
-   * @param deleteRetrySchedule The schedule by which to retry deleting the message (which should be rare)
+   * @param parallelism         how many messages are processed in parallel
+   * @param deleteRetrySchedule the schedule by which to retry deleting the message (which should be rare)
    * @param process             the function that takes an ObjectSummary and returns a ZIO effect
    * @tparam E the error type of the ZIO effect
    * @tparam A the value type of the ZIO effect
    * @return a ZStream that can either fail with a Throwable or emit values of type Either[(Message.ReadOnly, Cause[E]), A]
    */
-  def processMessages[E, A](url: String, settings: SqsStreamSettings,
-                            parallelism: Int = 1,
-                            deleteRetrySchedule: Schedule[Any, Any, zio.Duration] = defaultDeleteRetryPolicy)
-                           (process: Message.ReadOnly => ZIO[Any, E, A]): ZStream[Sqs, Throwable, Either[(Message.ReadOnly, Cause[E]), A]] =
-    zio.sqs.SqsStream(url, settings)
+  def processMessages[E, A](
+    url: String,
+    settings: SqsStreamSettings,
+    parallelism: Int = 1,
+    deleteRetrySchedule: Schedule[Any, Any, Any] = defaultDeleteRetryPolicy
+  )(process: Message.ReadOnly => ZIO[Any, E, A]): ZStream[Sqs, Throwable, Either[(Message.ReadOnly, Cause[E]), A]] =
+    zio.sqs
+      .SqsStream(url, settings)
       // Unordered since SQS (and queues in general) don't have ordering semantics
       .mapZIOParUnordered(parallelism) { msg =>
         process(msg)
-          // Capture any errors and defects
-          .sandbox
-          .zipLeft {
-            SqsStream.deleteMessage(url, msg)
-              // Retry in case it fails to delete transiently
-              .retry(defaultDeleteRetryPolicy)
-              // Die if it fails and stop the stream, to avoid reprocessing validly processed messages
-              // This would be very rare and caught early in testing.
-              .orDie
-          }.mapError(err => (msg, err))
-          .either // Move the error to the return value, so that it doesn't stop the stream
+        // Capture any errors and defects
+        .sandbox.zipLeft {
+          SqsStream
+            .deleteMessage(url, msg)
+            // Retry in case it fails to delete transiently
+            .retry(deleteRetrySchedule)
+            // Die if it fails and stop the stream, to avoid reprocessing validly processed messages
+            // This would be very rare and caught early in testing.
+            .orDie
+        }.mapError(err => (msg, err)).either // Move the error to the return value, so that it doesn't stop the stream
       }
 
-  private val defaultDeleteRetryPolicy: Schedule[Any, Any, zio.Duration] =
-    (Schedule.exponential(10.milliseconds) >>> Schedule.elapsed).whileOutput(_ < 30.seconds)
+  private val defaultDeleteRetryPolicy: Schedule[Any, Any, Any] =
+    Schedule.exponential(10.milliseconds) && Schedule.upTo(30.seconds)
 }
--- zio-sqs/src/test/scala/zio/sqs/ZioSqsMockServer.scala
+++ zio-sqs/src/test/scala/zio/sqs/ZioSqsMockServer.scala
@@ -1,7 +1,7 @@
 package zio.sqs
 
 import java.net.URI
-import zio.aws.core.config.AwsConfig
+import zio.aws.core.config.{ AwsConfig, CommonAwsConfig }
 import zio.aws.sqs.Sqs
 import org.elasticmq.rest.sqs.SQSRestServer
 import org.elasticmq.RelaxedSQSLimits
@@ -9,6 +9,7 @@ import org.elasticmq.rest.sqs.TheSQSRestServerBuilder
 import org.elasticmq.NodeAddress
 import software.amazon.awssdk.auth.credentials.{ AwsBasicCredentials, StaticCredentialsProvider }
 import software.amazon.awssdk.regions.Region
+import zio.aws.netty.NettyHttpClient
 import zio.{ Scope, ZIO, ZLayer }
 
 object ZioSqsMockServer extends TheSQSRestServerBuilder(None, None, "", 9324, NodeAddress(), true, RelaxedSQSLimits, "elasticmq", "000000000000", None) {
@@ -30,4 +31,46 @@ object ZioSqsMockServer extends TheSQSRestServerBuilder(None, None, "", 9324, No
         )
         .endpointOverride(uri)
     )
+}
+
+object MockSqsServerAndClient {
+
+  lazy val layer: ZLayer[Any, Throwable, Sqs] =
+    (NettyHttpClient.default ++ mockServer) >>> AwsConfig.configured() >>> zio.aws.sqs.Sqs.live
+
+  private lazy val mockServer: ZLayer[Any, Throwable, CommonAwsConfig] = {
+    val dummyAwsKeys =
+      StaticCredentialsProvider.create(AwsBasicCredentials.create("key", "key"))
+    val region       = Region.AP_NORTHEAST_2
+    ZLayer.scoped {
+      for {
+        // Random port to allow parallel tests
+        port          <- ZIO.randomWith(_.nextIntBetween(9000, 50000))
+        serverBuilder <- ZIO.attempt(
+                           TheSQSRestServerBuilder(
+                             providedActorSystem = None,
+                             providedQueueManagerActor = None,
+                             interface = "",
+                             port = port,
+                             serverAddress = NodeAddress(),
+                             generateServerAddress = true,
+                             sqsLimits = RelaxedSQSLimits,
+                             _awsRegion = region.toString,
+                             _awsAccountId = "000000000000",
+                             queueEventListener = None
+                           )
+                         )
+        server        <- ZIO.acquireRelease(
+                           ZIO.attempt(serverBuilder.start())
+                         )(server => ZIO.succeed(server.stopAndWait()))
+        awsConfig      = CommonAwsConfig(
+                           region = Some(region),
+                           credentialsProvider = dummyAwsKeys,
+                           endpointOverride = Some(new URI(s"http://localhost:${port}")),
+                           None
+                         )
+      } yield awsConfig
+    }
+  }
+
 }
